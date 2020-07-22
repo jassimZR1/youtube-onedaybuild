17e218da9a508af33c93adc082bb5b685f93315d
jassim <jassimhanif@hotmail.com>
2020-07-22 17:31:57 +0300

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Jassim Mohammed on 7/22/20.
+//  Copyright © 2020 Christopher Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
